c2241113099487d59235c378c17a38b119610d5e
dabinsi <leo@dabinsi.com>
2020-06-25 13:44:56 -0400

    initial commit with model-msg-view using single file

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
-// (Lines like the one below ignore selected Clippy rules
-//  - it's useful when you want to check your code with `cargo make verify`
-// but some rules are too "annoying" or are not applicable for your case.)
 #![allow(clippy::wildcard_imports)]
+
+use seed::{prelude::*, *};
+use std::collections::BTreeMap;
+use ulid::Ulid;
 
 use seed::{prelude::*, *};
 
@@ -11,7 +12,13 @@ use seed::{prelude::*, *};
 
 // `init` describes what should happen when your app started.
 fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
-    Model::default()
+    Model {
+        todos: BTreeMap::new(),
+        new_todo_title: String::new(),
+        selected_todo: None,
+        filter: Filter::All,
+        base_url: Url::new(),
+    }.add_mock_data()
 }
 
 // ------ ------
@@ -19,23 +26,129 @@ fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
 // ------ ------
 
 // `Model` describes our app state.
-type Model = i32;
+struct Model {
+    todos: BTreeMap<Ulid, Todo>,
+    new_todo_title: String,
+    selected_todo: Option<SelectedTodo>,
+    filter: Filter,
+    base_url: Url,
+}
+
+struct Todo {
+    id: Ulid,
+    title: String,
+    completed: bool,
+}
+
+struct SelectedTodo {
+    id: Ulid,
+    title: String,
+    input_element: ElRef<web_sys::HtmlInputElement>,
+}
+
+enum Filter {
+  All,
+  Active,
+  Completed,
+}
+
+// TODO: Remove
+impl Model {
+    fn add_mock_data(mut self) -> Self {
+        let (id_a, id_b) = (Ulid::new(), Ulid::new());
+        
+        self.todos.insert(id_a, Todo {
+            id: id_a,
+            title: "I'm todo A".to_owned(),
+            completed: false,
+        });
+
+        self.todos.insert(id_b, Todo {
+            id: id_b,
+            title: "I'm todo B".to_owned(),
+            completed: true,
+        });
+
+        self.new_todo_title = "I'm a new todo title".to_owned();
+
+        self.selected_todo = Some(SelectedTodo {
+            id: id_b,
+            title: "I'm better todo B".to_owned(),
+            input_element: ElRef::new(),
+        });
+        self
+    }
+}
 
 // ------ ------
 //    Update
 // ------ ------
 
-// (Remove the line below once any of your `Msg` variants doesn't implement `Copy`.)
-#[derive(Copy, Clone)]
-// `Msg` describes the different events you can modify state with.
 enum Msg {
-    Increment,
+    UrlChanged(subs::UrlChanged),
+    NewTodoTitleChanged(String),
+
+    // ------ Basic Todo operations ------
+
+    CreateTodo,
+    ToggleTodo(Ulid),
+    RemoveTodo(Ulid),
+    
+    // ------ Bulk operations ------
+
+    CheckOrUncheckAll,
+    ClearCompleted,
+    
+    // ------ Selection ------
+
+    SelectTodo(Option<Ulid>),
+    SelectedTodoTitleChanged(String),
+    SaveSelectedTodo,
 }
+
 
 // `update` describes how to handle each `Msg`.
 fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
     match msg {
-        Msg::Increment => *model += 1,
+        Msg::UrlChanged(subs::UrlChanged(url)) => {
+            log!("UrlChanged", url);
+        }
+        Msg::NewTodoTitleChanged(title) => {
+            log!("NewTodoTitleChanged", title);
+        }
+    
+        // ------ Basic Todo operations ------
+
+        Msg::CreateTodo => {
+            log!("CreateTodo");
+        }
+        Msg::ToggleTodo(id) => {
+            log!("ToggleTodo");
+        }
+        Msg::RemoveTodo(id) => {
+            log!("RemoveTodo");
+        }
+        
+        // ------ Bulk operations ------
+
+        Msg::CheckOrUncheckAll => {
+            log!("CheckOrUncheckAll");
+        }
+        Msg::ClearCompleted => {
+            log!("ClearCompleted");
+        }
+        
+        // ------ Selection ------
+
+        Msg::SelectTodo(opt_id) => {
+            log!("SelectTodo", opt_id);
+        },
+        Msg::SelectedTodoTitleChanged(title) => {
+            log!("SelectedTodoTitleChanged", title);
+        },
+        Msg::SaveSelectedTodo => {
+            log!("SaveSelectedTodo");
+        }
     }
 }
 
@@ -46,12 +159,8 @@ fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
 // (Remove the line below once your `Model` become more complex.)
 #[allow(clippy::trivially_copy_pass_by_ref)]
 // `view` describes what to display.
-fn view(model: &Model) -> Node<Msg> {
-    div![
-        "This is a counter: ",
-        C!["counter"],
-        button![model, ev(Ev::Click, |_| Msg::Increment),],
-    ]
+fn view(model: &Model) -> Vec<Node<Msg>> {
+    raw![include_str!("../template.html")]
 }
 
 // ------ ------
@@ -61,6 +170,12 @@ fn view(model: &Model) -> Node<Msg> {
 // (This function is invoked by `init` function in `index.html`.)
 #[wasm_bindgen(start)]
 pub fn start() {
-    // Mount the `app` to the element with the `id` "app".
-    App::start("app", init, update, view);
+    console_error_panic_hook::set_once();
+    
+    let root_element = document()
+        .get_elements_by_class_name("todoapp")
+        .item(0)
+        .expect("element with the class `todoapp`");
+
+    App::start(root_element, init, update, view);
 }
